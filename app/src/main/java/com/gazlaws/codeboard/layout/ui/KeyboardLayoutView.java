e4bff464182ab17f575f2e19a8c0ea6a6865a078
Eke Péter <peterekepeter@gmail.com>
2019-10-22 23:07:10 +0300

    Take display metrics when deciding on keyboard size.

--- app/src/main/java/com/gazlaws/codeboard/layout/ui/KeyboardLayoutView.java
+++ app/src/main/java/com/gazlaws/codeboard/layout/ui/KeyboardLayoutView.java
@@ -1,9 +1,11 @@
 package com.gazlaws.codeboard.layout.ui;
 
 import android.content.Context;
+import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewParent;
 
 import com.gazlaws.codeboard.theme.UiTheme;
 
@@ -14,41 +16,31 @@ import static android.content.ContentValues.TAG;
 
 public class KeyboardLayoutView extends ViewGroup {
 
-    int availableWidth = 0;
-    int availableHeight = 0;
-    boolean measured = false;
     private final UiTheme uiTheme;
 
     public KeyboardLayoutView(Context context, UiTheme uiTheme) {
         super(context);
         setBackgroundColor(uiTheme.backgroundColor);
         this.uiTheme = uiTheme;
-        Log.d(TAG, "KeyboardLayoutView: CREATED!!!");
     }
 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
 
-        final int widthMode = MeasureSpec.getMode(widthMeasureSpec);
-        final int heightMode = MeasureSpec.getMode(heightMeasureSpec);
-        final int widthSize = MeasureSpec.getSize(widthMeasureSpec);
-        final int heightSize = MeasureSpec.getSize(heightMeasureSpec);
+        DisplayMetrics metrics = getContext().getResources().getDisplayMetrics();
 
-        // when measure is called for first time store the width and height
-        if (!measured){
-            availableWidth = widthSize;
-            availableHeight = heightSize;
-            measured = true;
-        }
-        float size = availableHeight > availableWidth ? uiTheme.portraitSize : uiTheme.landscapeSize;
+        int availableHeight = metrics.heightPixels;
+        int availableWidth = metrics.widthPixels;
 
-        Log.d(TAG, "onMeasure" + widthSize + " " + heightSize);
-        setMeasuredDimension(availableWidth, (int)(availableHeight * size));
+        float keyboardSize = availableHeight > availableWidth
+                ? uiTheme.portraitSize
+                : uiTheme.landscapeSize;
+
+        setMeasuredDimension(availableWidth, (int)(availableHeight * keyboardSize));
     }
 
     @Override
     protected void onLayout(boolean changed, int l, int t, int r, int b) {
-        Log.d(TAG, "onLayout: " + l + " " + t + " " + r + " " + b);
         final int count = getChildCount();
         for (int i = 0; i < count; i++) {
             final View child = getChildAt(i);
@@ -66,7 +58,6 @@ public class KeyboardLayoutView extends ViewGroup {
         for (KeyboardButtonView button : getKeyboardButtons()){
             button.applyCtrlModifier(ctrlPressed);
         }
-
     }
 
     private Collection<KeyboardButtonView> getKeyboardButtons(){
