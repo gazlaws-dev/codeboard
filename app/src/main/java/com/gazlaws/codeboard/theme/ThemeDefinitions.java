96a765c48945ed2832149508c6cf732cfd675ef3
abrarishere <legendabrar44@gmail.com>
2024-06-05 18:50:41 +0500

    Added separate gradient for theme

--- app/src/main/java/com/gazlaws/codeboard/theme/ThemeDefinitions.java
+++ app/src/main/java/com/gazlaws/codeboard/theme/ThemeDefinitions.java
@@ -4,6 +4,8 @@ public class ThemeDefinitions {
 
     private static int whiteColor = 0xffffffff;
     private static int blackColor = 0xff000000;
+    private static int buttonStartColour = 0xff000000;
+    private static int buttonEndColour = 0xff00ffff; 
 
     public static ThemeInfo Default(){
         return MaterialDark();
@@ -19,8 +21,8 @@ public class ThemeDefinitions {
             14.0f,                  // Size
             16.0f,                  // Font size
             18.0f,                  // Landscape size
-            0x00000000,             // Button body start color
-            0x00009999              // Button body end color
+            buttonStartColour,      // Button body start color
+            buttonEndColour         // Button body end color
         );
     }
 
@@ -28,30 +30,46 @@ public class ThemeDefinitions {
         ThemeInfo theme = Default();
         theme.foregroundColor = blackColor;
         theme.backgroundColor = 0xffeceff1;
+        theme.buttonStartColour = 0xff000000;
+        theme.buttonEndColour = 0xff00ffff;
         return theme;
     }
 
     public static ThemeInfo PureBlack(){
         ThemeInfo theme = MaterialDark();
+        theme.foregroundColor = whiteColor;
         theme.backgroundColor = blackColor;
+        theme.buttonStartColour = 0xff000000; // Black
+        theme.buttonEndColour = 0xff00ffff; // Color = Cyan
         return theme;
     }
 
     public static ThemeInfo White(){
         ThemeInfo theme = MaterialWhite();
         theme.backgroundColor = whiteColor;
+        theme.buttonStartColour = 0xff000000; // Black
+        theme.buttonEndColour = 0xff00feff; // Color = Cyan
         return theme;
     }
 
     public static ThemeInfo Blue(){
         ThemeInfo theme = MaterialDark();
         theme.backgroundColor = 0xff0d47a1;
+        // For blue theme, we will use white color as button text color
+        theme.foregroundColor = whiteColor;
+        theme.buttonStartColour = 0xff000000; // Black  
+        theme.buttonEndColour = 0xff00ffff; // Cyan
         return theme;
     }
 
     public static ThemeInfo Purple(){
         ThemeInfo theme = MaterialDark();
         theme.backgroundColor = 0xff4a148c;
+        // For purple theme, we will use white color as button text color
+        // and black color as button background color
+        theme.foregroundColor = whiteColor;
+        theme.buttonStartColour = 0xff000000; // Black
+        theme.buttonEndColour = 0xff00ffff; // Cyan
         return theme;
     }
 }
