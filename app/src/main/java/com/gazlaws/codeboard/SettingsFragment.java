4f5f9352c0cfac032b9995f40b9c4dec902c1eb2
Muhammad Abrar <legendabrar44@gmail.com>
2024-07-02 17:45:10 +0500

    ok

--- app/src/main/java/com/gazlaws/codeboard/SettingsFragment.java
+++ app/src/main/java/com/gazlaws/codeboard/SettingsFragment.java
@@ -251,7 +251,7 @@ public class SettingsFragment extends PreferenceFragmentCompat implements IOnFoc
         }
         keyboardPreferences.setBgColor(String.valueOf(themeInfo.backgroundColor));
         keyboardPreferences.setFgColor(String.valueOf(themeInfo.foregroundColor));
-        // Work by gpt below 2 lines
+        // Work by gpt 2 lines
         keyboardPreferences.setGradientStartColor(themeInfo.buttonBodyStartColor);
         keyboardPreferences.setGradientEndColor(themeInfo.buttonBodyEndColor);
     }
